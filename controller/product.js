0f32db4d2aa6e51aea37987bcb208977358a4e38
Madhura K S <59435697+MadhuKS-MKS@users.noreply.github.com>
2020-09-19 17:58:17 +0530

    Add files via upload

--- controller/product.js
+++ controller/product.js
@@ -11,18 +11,14 @@ exports.getProducts = asyncHandler(async (req, res, next) => {
   res.status(200).json(res.advancedResults);
 });
 
-
-
 // @desc      Get single product
 // @route     GET api/products/:productId
 // @access    Public
 exports.getProduct = asyncHandler(async (req, res, next) => {
-  const product = await Products.findById(req.params.ProductId)
-    .populate({
-      path: "category",
-      select: "catname"
-    });
-
+  const product = await Products.findById(req.params.ProductId).populate({
+    path: "category",
+    select: "catname",
+  });
 
   if (!product) {
     return next(
@@ -41,7 +37,6 @@ exports.getProduct = asyncHandler(async (req, res, next) => {
 // @route     POST api/products
 // @access    Private
 exports.addProduct = asyncHandler(async (req, res, next) => {
-
   const product = await Products.create(req.body);
   res.status(200).json({
     success: true,
@@ -61,8 +56,6 @@ exports.updateProduct = asyncHandler(async (req, res, next) => {
       404
     );
   }
-
-
 
   product = await Products.findByIdAndUpdate(req.params.ProductId, req.body, {
     new: true,
@@ -88,7 +81,6 @@ exports.deleteProduct = asyncHandler(async (req, res, next) => {
     );
   }
 
-
   await product.remove();
 
   res.status(200).json({
@@ -101,42 +93,38 @@ exports.deleteProduct = asyncHandler(async (req, res, next) => {
 // @route     POST /api/products/photo
 // @access    Private
 exports.productPhotoUpload = asyncHandler(async (req, res, next) => {
-  // if (!req.files) {
-  //   return next(new ErrorResponse(`Please upload a file`, 400));
-  // }
+  if (!req.files) {
+    return next(new ErrorResponse(`Please upload a file`, 400));
+  }
 
-  // const file = req.files.file;
+  const file = req.files.file;
 
   // Make sure the image is a photo
-  // if (!file.mimetype.startsWith("image")) {
-  //   return next(new ErrorResponse(`Please upload an image file`, 400));
-  // }
+  if (!file.mimetype.startsWith("image")) {
+    return next(new ErrorResponse(`Please upload an image file`, 400));
+  }
 
   // Check filesize
-  // if (file.size > process.env.MAX_FILE_UPLOAD) {
-  //   return next(
-  //     new ErrorResponse(
-  //       `Please upload an image less than ${process.env.MAX_FILE_UPLOAD}`,
-  //       400
-  //     )
-  //   );
-  // }
+  if (file.size > process.env.MAX_FILE_UPLOAD) {
+    return next(
+      new ErrorResponse(
+        `Please upload an image less than ${process.env.MAX_FILE_UPLOAD}`,
+        400
+      )
+    );
+  }
 
-  // file.mv(
-  //   `${__dirname}/uploads/${file.name}`,
-  //   async (err) => {
-  //     if (err) {
-  //       console.error(err);
-  //       return next(new ErrorResponse(`Problem with file upload`, 500));
-  //     }
+  file.mv(`${__dirname}/../uploads/${file.name}`, async (err) => {
+    if (err) {
+      console.error(err);
+      return next(new ErrorResponse(`Problem with file upload`, 500));
+    }
 
-  //     const files = `/uploads/${file.name}`;
+    const files = `/VegieeKing/uploads/${file.name}`;
 
-  // res.status(200).json({
-  //   // success: true,
-  //   data: req.file.files,
-  // });
-  //   }
-  // );
-  console.log(req.body)
+    res.status(200).json({
+      // success: true,
+      data: files,
+    });
+  });
 });
